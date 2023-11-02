c8969ac412e5091b3e44323384c3ccbb0a18a383
maiisoo <ngmaianh187.work@gmail.com>
2023-11-03 01:29:22 +0700

    create a blank page

---
+++ frontend/src/pages/BlankPage.js
@@ -0,0 +1,8 @@
+import { Link as RouterLink } from 'react-router-dom';
+import Page from '../components/Page'
+
+export default function BlankPage() {
+  return (
+    <Page title="Blank Page"/>
+  );
+}

af2e593edd21d98c2badda877da8b99f4bd0190d
maiisoo <ngmaianh187.work@gmail.com>
2023-11-26 15:40:28 +0700

    category page done, unfinished edit profile

--- frontend/src/pages/dashboard/general/Categories.js
+++ frontend/src/pages/dashboard/general/Categories.js
@@ -1,37 +1,86 @@
 // @mui
-import { Container, Typography } from '@mui/material';
-// hooks
-import useSettings from "../../../hooks/useSettings";
-// components
-import Page from "../../../components/Page";
+import { Grid, Card, CardContent, Typography, Link } from '@mui/material';
+// image
+import CategoryImage1 from '../../../img/Category_1.png';
+import CategoryImage2 from '../../../img/Category_2.png';
+import CategoryImage3 from '../../../img/Category_3.png';
+import CategoryImage4 from '../../../img/Category_4.png';
+
+export default function CategoriesPage() {
+  const categories = [
+    { title: 'Arabic poetry', imagePath: CategoryImage1, link: '#category_1' },
+    { title: 'Learning korean grammar', imagePath: CategoryImage2, link: '#category_2' },
+    { title: 'Book', imagePath: CategoryImage3, link: '#category_3' },
+    { title: 'Art', imagePath: CategoryImage4, link: '#category_4' },
+  ];
+  const popularCategories = [
+    { title: 'Arabic poetry', imagePath: CategoryImage1, link: '#category_1' },
+    { title: 'Learning korean grammar', imagePath: CategoryImage2, link: '#category_2' },
+    { title: 'Book', imagePath: CategoryImage3, link: '#category_3' },
+    { title: 'Art', imagePath: CategoryImage4, link: '#category_4' },
+  ];
+
+  return (
+
+    <Grid container justifyContent="center">
+      <Grid item xs={12} md={8}>
+        <Grid container spacing={2}>
+          {categories.map((category, index) => (
+            <Grid item xs={12} sm={6} key={index}>
+              <Link href={category.link} underline="none">
+                <Card>
+                  <div style={{ position: 'relative' }}>
+                    <img
+                      src={category.imagePath}
+                      alt={category.title}
+                      style={{ width: '100%', height: '100%', objectFit: 'cover', aspectRatio: '3/1'}}
+                    />
+                    <CardContent style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+                      <Typography variant="h6" component="h2" style={{ color: 'white', fontFamily: 'Arial, san-serif' }}>
+                        {category.title}
+                      </Typography>
+                    </CardContent>
+                  </div>
+                </Card>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} md={8}>
+        <Typography variant="subtitle1" component="h2" align="center" style={{ color: '#797777', margin: '20px 0' }}>
+          Popular on Picsta
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} md={8}>
+        <Grid container spacing={2}>
+          {popularCategories.map((category, index) => (
+            <Grid item xs={12} sm={6} key={index}>
+              <Link href={category.link} underline="none">
+                <Card>
+                  <div style={{ position: 'relative' }}>
+                    <img
+                      src={category.imagePath}
+                      alt={category.title}
+                      style={{ width: '100%', height: '100%', objectFit: 'cover', aspectRatio: '3/1' }}
+                    />
+                    <CardContent style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+                      <Typography variant="h6" component="h2" style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>
+                        {category.title}
+                      </Typography>
+                    </CardContent>
+                  </div>
+                </Card>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 
-// ----------------------------------------------------------------------
 
-export default function Home() {
-    const { themeStretch } = useSettings();
-
-    return (
-        <Page title="Home">
-            <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Typography variant="h3" component="h1" paragraph>
-                    Page Template
-                </Typography>
-                <Typography gutterBottom>
-                    Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod
-                    ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Phasellus blandit leo
-                    ut odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id
-                    purus. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. In consectetuer turpis ut velit.
-                    Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-                    Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
-                </Typography>
-                <Typography>
-                    Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut metus varius laoreet. Curabitur
-                    ullamcorper ultricies nisi. Ut non enim eleifend felis pretium feugiat. Donec mi odio, faucibus at,
-                    scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque libero metus, condimentum nec, tempor a, commodo
-                    mollis, magna. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Cras dapibus.
-                </Typography>
-            </Container>
-        </Page>
-    );
-}
--- frontend/src/pages/dashboard/profile/EditProfile.js
+++ frontend/src/pages/dashboard/profile/EditProfile.js
@@ -1,37 +1,113 @@
+import React from 'react'
+import { paramCase, capitalCase } from 'change-case';
+import { useParams, useLocation } from 'react-router-dom';
 // @mui
-import { Container, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Tab, Box, Card, Tabs, Container } from '@mui/material';
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths';
 // hooks
-import useSettings from "../../../hooks/useSettings";
+import useSettings from '../../../hooks/useSettings';
+import useAuth from '../../../hooks/useAuth';
+import useTabs from '../../../hooks/useTabs';
+// _mock_
+import { _userAbout, _userFeeds, _userGallery, _userFollowers, _userList } from '../../../_mock';
 // components
-import Page from "../../../components/Page";
+import Page from '../../../components/Page';
+import { MotionContainer } from '../../../components/animate';
+import Iconify from '../../../components/Iconify';
+import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs';
+import UserNewEditForm from '../../../sections/@dashboard/user/UserNewEditForm'
 
+// sections
+import {
+  Profile,
+  ProfileCover,
+  ProfileGallery,
+  ProfileFollowers,
+} from '../../../sections/@dashboard/user/profile';
+
+import {
+  AccountGeneral,
+  AccountChangePassword,
+  AccountNotifications,
+} from '../../../sections/@dashboard/user/account';
 
 // ----------------------------------------------------------------------
 
-export default function Home() {
-    const { themeStretch } = useSettings();
+const RootStyle = styled('div')(({ theme }) => ({
+  display: 'flex',
+  height: '100%',
+  alignItems: 'center',
+  paddingTop: theme.spacing(15),
+  paddingBottom: theme.spacing(10),
+}));
 
-    return (
-        <Page title="Home">
-            <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Typography variant="h3" component="h1" paragraph>
-                    Page Template
-                </Typography>
-                <Typography gutterBottom>
-                    Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod
-                    ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Phasellus blandit leo
-                    ut odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id
-                    purus. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. In consectetuer turpis ut velit.
-                    Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-                    Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
-                </Typography>
-                <Typography>
-                    Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut metus varius laoreet. Curabitur
-                    ullamcorper ultricies nisi. Ut non enim eleifend felis pretium feugiat. Donec mi odio, faucibus at,
-                    scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque libero metus, condimentum nec, tempor a, commodo
-                    mollis, magna. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Cras dapibus.
-                </Typography>
-            </Container>
-        </Page>
-    );
+// ----------------------------------------------------------------------
+
+const TabsWrapperStyle = styled('div')(({ theme }) => ({
+  zIndex: 9,
+  bottom: 0,
+  width: '100%',
+  display: 'flex',
+  position: 'absolute',
+  backgroundColor: theme.palette.background.paper,
+  [theme.breakpoints.up('sm')]: {
+    justifyContent: 'center',
+  },
+  [theme.breakpoints.up('md')]: {
+    justifyContent: 'flex-end',
+    paddingRight: theme.spacing(3),
+  },
+}));
+
+// ----------------------------------------------------------------------
+
+export default function UserEditProfile() {
+  const { themeStretch } = useSettings()
+  const { user } = useAuth()
+  const { currentTab, onChangeTab } = useTabs('profile');
+  const PROFILE_TABS = [
+    {
+      value: 'profile',
+      icon: <Iconify icon={'ic:round-account-box'} width={20} height={20} />,
+      component: <Profile myProfile={_userAbout} posts={_userFeeds} />,
+    },
+    {
+        value: 'followers',
+        icon: <Iconify icon={'eva:heart-fill'} width={20} height={20} />,
+        component: <ProfileFollowers followers={_userFollowers} />,
+      },
+    {
+        value: 'gallery',
+        icon: <Iconify icon={'ic:round-perm-media'} width={20} height={20} />,
+        component: <ProfileGallery gallery={_userGallery} />,
+      },
+    ];
+
+      const { pathname } = useLocation();
+
+      const { name = '' } = useParams();
+
+      const isEdit = pathname.includes('edit');
+
+      const currentUser = _userList.find((user) => paramCase(user.name) === name);
+  return(
+    <Page title="Edit Profile">
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        <Card
+          sx={{
+            mb: 3,
+              height: 280,
+              position: 'relative',
+          }}
+        >
+        <ProfileCover myProfile={_userAbout} />
+        </Card>
+        <AccountGeneral />
+        <AccountChangePassword />
+        <AccountNotifications />
+      </Container>
+    </Page>
+  );
 }

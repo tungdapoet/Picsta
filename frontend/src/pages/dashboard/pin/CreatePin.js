1368ab3bce3f93c3a8f2687f6b9446d49fd520b6
doanminhquan <doanquanminh00@gmail.com>
2023-11-27 14:06:56 +0700

    createPin

--- frontend/src/pages/dashboard/pin/CreatePin.js
+++ frontend/src/pages/dashboard/pin/CreatePin.js
@@ -16,25 +16,23 @@ export default function Home() {
 
     return (
         <Page title="Home">
-        <Container maxWidth={themeStretch ? false : 'xl'}>
-            <Typography variant="h3" component="h1" paragraph>
-                Page Template
-            </Typography>
-            <Typography gutterBottom>
-                Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod
-                ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Phasellus blandit leo
-                ut odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id
-                purus. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. In consectetuer turpis ut velit.
-                Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-                Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
-            </Typography>
-            <Typography>
-                Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut metus varius laoreet. Curabitur
-                ullamcorper ultricies nisi. Ut non enim eleifend felis pretium feugiat. Donec mi odio, faucibus at,
-                scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque libero metus, condimentum nec, tempor a, commodo
-                mollis, magna. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Cras dapibus.
-            </Typography>
-        </Container>
-    </Page>
+            <Container maxWidth={themeStretch ? false : 'xl'}>
+                <Image 
+                    disabledEffect
+                    src={anh1}
+                    sx={{ width: 403, height: 273,
+                    left: 500,
+                    position: 'absolute'}} />
+                <Typography sx={{ left: 600, top: 440, position: 'absolute', fontSize: 25, fontFamily: 'Roboto', fontWeight: '600'
+                    }}>Start creating Pins</Typography>
+                <Button sx={{ width: 118, height: 48,
+                    left: 320, top: 400
+                    }}
+                    href='http://localhost:3030/dashboard/pin/detail'
+                    >
+                        Creat Pin
+                </Button>
+            </Container>
+        </Page>
     );
 }

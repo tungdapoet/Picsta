6a2449ed0da5cf76b5d219bcd0999fa0ce947684
maiisoo <ngmaianh187.work@gmail.com>
2023-12-02 15:41:03 +0700

    update edit profile

--- frontend/src/pages/dashboard/profile/EditProfile.js
+++ frontend/src/pages/dashboard/profile/EditProfile.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { paramCase, capitalCase } from 'change-case';
 import { useParams, useLocation } from 'react-router-dom';
 // @mui
-import { styled } from '@mui/material/styles';
-import { Tab, Box, Card, Tabs, Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Box, Card, Container, Typography, Avatar, Grid, Switch, FormControlLabel } from '@mui/material';
 // routes
 import { PATH_DASHBOARD } from '../../../routes/paths';
 // hooks
@@ -14,10 +14,6 @@ import useTabs from '../../../hooks/useTabs';
 import { _userAbout, _userFeeds, _userGallery, _userFollowers, _userList } from '../../../_mock';
 // components
 import Page from '../../../components/Page';
-import { MotionContainer } from '../../../components/animate';
-import Iconify from '../../../components/Iconify';
-import HeaderBreadcrumbs from '../../../components/HeaderBreadcrumbs';
-import UserNewEditForm from '../../../sections/@dashboard/user/UserNewEditForm'
 
 // sections
 import {
@@ -35,55 +31,82 @@ import {
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  display: 'flex',
-  height: '100%',
-  alignItems: 'center',
-  paddingTop: theme.spacing(15),
-  paddingBottom: theme.spacing(10),
-}));
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF6636', // Change this to your desired primary color
+      contrastText: '#FFFFFF', // Set contrastText to white for primary button text
+    },
+  },
+  // other theme configurations
+});
 // ----------------------------------------------------------------------
 
-const TabsWrapperStyle = styled('div')(({ theme }) => ({
-  zIndex: 9,
-  bottom: 0,
-  width: '100%',
-  display: 'flex',
-  position: 'absolute',
-  backgroundColor: theme.palette.background.paper,
-  [theme.breakpoints.up('sm')]: {
-    justifyContent: 'center',
-  },
-  [theme.breakpoints.up('md')]: {
-    justifyContent: 'flex-end',
-    paddingRight: theme.spacing(3),
-  },
-}));
+const ProfileHeader = () => {
+    const postsCount = 120; // Số bài đăng
+      const followersCount = 3400; // Số người theo dõi
+      const followingCount = 300; // Số người đang theo dõi
+  return (
+    <Box sx={{ flexGrow: 1, p: 2, backgroundColor: 'background.paper', borderRadius: '8px', textAlign: 'center' }}>
+      <Avatar alt="User Name" src="/path-to-avatar.jpg" sx={{ width: 90, height: 90, mx: 'auto', mb: 2 }} />
+      <Typography variant="h6" component="div">
+        meof maajp
+      </Typography>
+      <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
+        ghét chiến tranh yêu hòa bình yêu bó cầu ghét điều hậu
+      </Typography>
+      {/* Stats Section của Qhg */}
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ textAlign: 'center', px: 2, borderRight: 1, borderColor: 'black' }}>
+          <Typography sx={{ fontSize: '1.25rem' }}>{postsCount}</Typography>
+          <Typography variant="body2" color="text.secondary">Shots</Typography>
+        </Box>
+        <Box sx={{ textAlign: 'center', px: 2, borderRight: 1, borderColor: 'black' }}>
+          <Typography sx={{ fontSize: '1.25rem' }}>{followersCount}</Typography>
+          <Typography variant="body2" color="text.secondary">Followers</Typography>
+        </Box>
+        <Box sx={{ textAlign: 'center', px: 2 }}>
+          <Typography sx={{ fontSize: '1.25rem' }}>{followingCount}</Typography>
+          <Typography variant="body2" color="text.secondary">Following</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 // ----------------------------------------------------------------------
+const SocialConnect = () => {
+  return (
+  <ThemeProvider theme={theme}>
+    <Grid container spacing={3} justifyContent="flex-end" sx={{ pt: 3 }} >
+      <Grid item xs={12} md={8}>
+        <Typography variant="h6" sx={{ marginTop: 2 }}>
+          Social Connect
+        </Typography>
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Facebook Connect"
+        />
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Google+ Connect"
+        />
+        <FormControlLabel
+          control={<Switch defaultChecked />}
+          label="Notifications"
+        />
+      </Grid>
+    </Grid>
+  </ThemeProvider>
+  );
+}
+// ----------------------------------------------------------------------
 
-export default function UserEditProfile() {
+
+export default function EditProfile() {
   const { themeStretch } = useSettings()
   const { user } = useAuth()
   const { currentTab, onChangeTab } = useTabs('profile');
-  const PROFILE_TABS = [
-    {
-      value: 'profile',
-      icon: <Iconify icon={'ic:round-account-box'} width={20} height={20} />,
-      component: <Profile myProfile={_userAbout} posts={_userFeeds} />,
-    },
-    {
-        value: 'followers',
-        icon: <Iconify icon={'eva:heart-fill'} width={20} height={20} />,
-        component: <ProfileFollowers followers={_userFollowers} />,
-      },
-    {
-        value: 'gallery',
-        icon: <Iconify icon={'ic:round-perm-media'} width={20} height={20} />,
-        component: <ProfileGallery gallery={_userGallery} />,
-      },
-    ];
 
       const { pathname } = useLocation();
 
@@ -93,6 +116,7 @@ export default function UserEditProfile() {
 
       const currentUser = _userList.find((user) => paramCase(user.name) === name);
   return(
+    <ThemeProvider theme={theme}>
     <Page title="Edit Profile">
       <Container maxWidth={themeStretch ? false : 'lg'}>
         <Card
@@ -102,12 +126,20 @@ export default function UserEditProfile() {
               position: 'relative',
           }}
         >
-        <ProfileCover myProfile={_userAbout} />
+        <ProfileHeader/>
         </Card>
-        <AccountGeneral />
-        <AccountChangePassword />
-        <AccountNotifications />
+        <AccountGeneral/>
+        <Grid container spacing={3} justifyContent="flex-end" sx={{ pt: 3 }} >
+          <Grid item xs={12} md={8}>
+            <Typography variant="h6" sx={{ marginTop: 2 }}>
+              Change Password
+            </Typography>
+            <AccountChangePassword/>
+          </Grid>
+        </Grid>
+        <SocialConnect/>
       </Container>
     </Page>
+    </ThemeProvider>
   );
 }
